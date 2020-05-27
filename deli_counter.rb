9db3ff88dda3bb2b177a86c5467c7f4318023052
Daniel Andrew <danwandrew@gmail.com>
2020-05-27 09:31:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,9 @@ def take_a_number(line, name)
   line << name
   puts "Welcome, #{name}. You are number #{line.length} in line."
 end
+
+def now_serving(line)
+ if line.length>=1;
+   puts ""
+else
+end
