1b28d2119d958c5e6ec42b0d0c8e3131eb1c881d
Daniel Andrew <danwandrew@gmail.com>
2020-05-27 09:24:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@
 def line(line) 
   if line.length>=1
     line_array =[]
-    line.each_with_index{|n,i| puts "The line is currently: #{i+1}. #{n}"}
+    line.each_with_index{|n,i| line_array<< [i+1, n]}
+    puts line_array
   else puts "The line is currently empty."
 end
 end
